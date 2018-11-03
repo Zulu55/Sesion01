61a95d4c874c236b3875c2034ec316425f551e2a
Juan Carlos Zuluaga Cardona <jzuluaga55@hotmail.com>
2018-11-03 12:31:54 -0500

    Finish

---
+++ Sesion01/CompositedProduct.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sesion01
+{
+    public class CompositedProduct : Product
+    {
+        #region Properties
+        public List<Product> Products { get; set; }
+        #endregion
+
+        #region Constructors
+        public CompositedProduct(int productId, string name, List<Product> products) : base(productId, name)
+        {
+            this.Products = products;
+        }
+        #endregion
+
+        #region Methods
+        public override decimal GetPrice()
+        {
+            decimal total = 0;
+            foreach (var product in this.Products)
+            {
+                total += product.GetPrice();
+            }
+
+            return total;
+        }
+
+        public override decimal GetTax()
+        {
+            decimal total = 0;
+            foreach (var product in this.Products)
+            {
+                total += product.GetTax();
+            }
+
+            return total;
+        }
+
+        public override string ToString()
+        {
+            var products = string.Empty;
+            foreach (var product in this.Products)
+            {
+                products += $"{product.Name}, ";
+            }
+
+            return $"{base.ToString()}\n\t" +
+                $"Products..........: {products}\n\t" +
+                $"Tax...............: {this.GetTax(),12:C2}\n\t" +
+                $"Value to pay......: {this.GetPrice(),12:C2}";
+        }
+        #endregion
+    }
+}
--- Sesion01/Program.cs
+++ Sesion01/Program.cs
@@ -8,6 +8,7 @@
         static void Main(string[] args)
         {
             var products = new List<Product>();
+            var combo = new List<Product>();
             var product1 = new ProductFixPrice(100, "Leche", 2300m, .17f);
             var product2 = new ProductFixPrice(200, "Pan", 1200m, .17f);
             var product3 = new ProductVariablePrice(300, "Mango Tomy", "Kilo", 5200m, 1.543f, .1f);
@@ -17,6 +18,14 @@
             products.Add(product2);
             products.Add(product3);
             products.Add(product4);
+            combo.Add(product1);
+            combo.Add(product2);
+            combo.Add(product3);
+            combo.Add(product4);
+
+            var product5 = new CompositedProduct(500, "Ancheta Especial", combo);
+
+            products.Add(product5);
 
             decimal value = 0;
             decimal tax = 0;
